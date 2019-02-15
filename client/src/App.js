feaa011cdf159bd23095b7e3d336a7d001a2bde9
pholmes10 <pholmes10@gmail.com>
2019-02-15 17:53:43 -0600

    adding tests for props and state using hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
+      persons: [
+        {name: "Max", age: 82},
+        {name: "Manu", age: 22},
+        {name: "Pat", age: 25}
+      ],
+      otherState: 'some other value'
+    });
+
+    console.log(personsState);
+    
+
+    const switchNameHandler = () => {
+      console.log('was clicked')
+      setPersonsState({persons: [
+        {name: "testing", age: 82},
+        {name: "Manu", age: 22},
+        {name: "Pat", age: 30}
+      ]
+    })
+    }
+
     return (
       <div className="App">
         <h1>Hello</h1>
+        <p>This is really working!</p>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>My Hobbies: Racing</Person>
       </div>
     );
   }
-}
 
-export default App;
+export default app;
+
